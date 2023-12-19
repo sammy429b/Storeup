2e4be441e4a636a65409723c62d04b0f9e2b74bb
Sammy429b <samarthnbahirgonde49@gmail.com>
2023-12-19 16:45:21 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar'
+import Index from './pages/Index'
+
+function App() {
+  return (
+    <>
+  <Navbar/>
+    <Index/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Index.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+
+function Index() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+  return <></>;
+}
+
+export default Index;
